79a1e43839a7025217a525c638c616552fa96dbd
Zakharov1van <ivan.ser.zak@gmail.com>
2023-09-26 15:36:46 +0300

    Improvements for 4th and 6th tasks

--- Lesson_3/4_StringComparison.py
+++ Lesson_3/4_StringComparison.py
@@ -1,6 +1,3 @@
 first_string = input("Enter your first string: ")
 second_string = input("Enter your second string: ")
-if first_string == second_string:
-    print(f"It's {first_string == second_string}, your strings are the same")
-else:
-    print(f"It's {first_string == second_string}, your strings are different")
+print(f"Is string the same?: {first_string == second_string}")
