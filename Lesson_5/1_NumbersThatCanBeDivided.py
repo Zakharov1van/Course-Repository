96713aaceabb7d86f49ba5d1b6aee483ee38c49a
Zakharov1van <ivan.ser.zak@gmail.com>
2023-10-06 20:09:52 +0300

    Added changes to simlify code

--- Lesson_5/1_NumbersThatCanBeDivided.py
+++ Lesson_5/1_NumbersThatCanBeDivided.py
@@ -8,11 +8,11 @@ number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 if user_number == 0:
     print("You can't enter 0")
 else:
-    idx = 0
+    is_divided = []
     for number in number_list:
         if number % user_number == 0:
             print(number)
-            idx += 1
+            is_divided.append(number)
 
-    if idx == 0:
+    if len(is_divided) == 0:
         print("There are no such numbers")
