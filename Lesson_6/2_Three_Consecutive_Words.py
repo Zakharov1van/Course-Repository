612ecb1b748d98284b43d031e8584e4ad6b8c664
Zakharov1van <ivan.ser.zak@gmail.com>
2023-10-08 19:28:30 +0300

    Added additional solution for 2nd task

--- Lesson_6/2_Three_Consecutive_Words.py
+++ Lesson_6/2_Three_Consecutive_Words.py
@@ -1,8 +1,16 @@
 user_str = "hello 1 one two three 15 world"
-user_lst = user_str.split(" ")
+user_lst = user_str.split()
 is_consecutive = False
 for item in range(1, len(user_lst)-1):
     if user_lst[item-1].isalpha() and user_lst[item].isalpha() and user_lst[item+1].isalpha():
         is_consecutive = True
 
+# words = 0
+# for item in user_lst:
+#     if words == 3:
+#         is_consecutive = True
+#     if item.isalpha():
+#         words += 1
+#     else:
+#         words = 0
 print(f"Are there three consecutive words in the string?: {is_consecutive}")
