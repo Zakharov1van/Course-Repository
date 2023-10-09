f316a6f046683cbd2de1f14604c0287efba2a27d
Zakharov1van <ivan.ser.zak@gmail.com>
2023-10-09 15:21:29 +0300

    Little change for 4th task

--- Lesson_7/4_Check_For_Repeated_Numbers.py
+++ Lesson_7/4_Check_For_Repeated_Numbers.py
@@ -1,4 +1,4 @@
-numbers_str = input("Enter the list of numbers: ")
+numbers_str = input("Enter the list of numbers (separated by space): ")
 numbers_lst = numbers_str.split()
 if len(numbers_lst) == len(set(numbers_lst)):
     print("All numbers are unique")
