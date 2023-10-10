c768ab6d276900c0fc95d858a531da3547ef02a8
Zakharov1van <ivan.ser.zak@gmail.com>
2023-10-10 15:24:43 +0300

    Added different method of printing result in 3rd task

--- Lesson_7/3_Unique_Elements_of_First_List.py
+++ Lesson_7/3_Unique_Elements_of_First_List.py
@@ -3,7 +3,9 @@ second_lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 first_set = set(first_lst)
 second_set = set(second_lst)
-result_lst = []
-for i in first_set.difference(second_set):
-    result_lst.append(str(i))
-print(f"Elements that exist only in 1st list: {', '.join(result_lst)}")
+# result_lst = []
+# for i in first_set.difference(second_set):
+#     result_lst.append(str(i))
+# print(f"Elements that exist only in 1st list: {', '.join(result_lst)}")
+
+print(*first_set.difference(second_set))
