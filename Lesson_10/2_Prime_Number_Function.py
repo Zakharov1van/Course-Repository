7a6e2f02ecf678314602e61eae5864a5a16491bc
Zakharov1van <ivan.ser.zak@gmail.com>
2023-10-21 20:59:16 +0300

    Added if statement into the function

--- Lesson_10/2_Prime_Number_Function.py
+++ Lesson_10/2_Prime_Number_Function.py
@@ -1,14 +1,14 @@
 def is_prime(number):
-    flag = True
-    for num in range(2, number):
-        if number % num == 0:
-            flag = False
-            break
-    return flag
+    if number < 2 or number > 1000:
+        raise ValueError("Your number must be between 2 and 1000")
+    else:
+        flag = True
+        for num in range(2, number):
+            if number % num == 0:
+                flag = False
+                break
+        return flag
 
 
 user_number = int(input("Enter your number: "))
-if user_number < 2 or user_number > 1000:
-    print("Your number must be between 2 and 1000")
-else:
-    print(f"Is {user_number} a prime number?: {is_prime(user_number)}")
+print(f"Is {user_number} a prime number?: {is_prime(user_number)}")
