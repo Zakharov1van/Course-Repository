9e1017e980ff7b58e78decac66475b409c8b9670
Zakharov1van <ivan.ser.zak@gmail.com>
2023-12-09 01:23:23 +0200

    Added closing browser after each test

--- Lesson_21/test/test_selenium.py
+++ Lesson_21/test/test_selenium.py
@@ -9,6 +9,7 @@ def test_base_url(base_url):
     page = MySelenium()
     page.get_page(base_url)
     assert page.get_current_url() == "https://www.google.com.ua/?hl=uk"
+    page.close_browser()
 
 
 @pytest.mark.parametrize("base_url, param", [("https://www.google.com", "Hello, world!")])
@@ -17,6 +18,7 @@ def test_search_box(base_url, param):
     page.get_page(base_url)
     text_box = page.search_box(param)
     assert text_box == "Hello, world!"
+    page.close_browser()
 
 
 @pytest.mark.parametrize("base_url, tag", [("https://www.google.com.ua/?hl=uk", "a")])
@@ -25,6 +27,7 @@ def test_all_links(base_url, tag):
     page.get_page(base_url)
     found_links = page.search_links_by_tag(tag) + page.search_iframe_links()
     assert found_links == link_list
+    page.close_browser()
 
 
 @pytest.mark.parametrize("base_url, tag", [("https://www.google.com.ua/?hl=uk", "a")])
@@ -35,3 +38,4 @@ def test_click(base_url, tag):
     found_links.append(page.get_current_url())
     page_titles = page.clicker_and_title_checker(found_links)
     assert page_titles == title_list
+    page.close_browser()
